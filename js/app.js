cb8d3de7802e4b935b913707c64217669fefe799
tuanpham <tuanpham@gcs-vn.com>
2019-05-08 09:21:03 +0700

    Update view

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
-import Router from "./router.js"
+import Router from "./router.js";
 
 var appvm = new Vue({
-    router: Router.router,
-    el: '#app',
-    data() {
-        return {
-            title: "Phạm Tuân's Blog",
-            message: "Welcome to Phạm Tuân's Blog"
-        }
-    },
-    components: {
-        "main-nav": () => import('./components/navigation.js'),
-        "toast-noti": () => import('./components/toast.js'),
-    },
-    created() {
-        console.log('Vue-App initialized: ' + this.$data.title);
-    },
+  router: Router.router,
+  el: "#app",
+  data() {
+    return {
+      title: "Phạm Tuân's Blog",
+      message: "Welcome to Phạm Tuân's Blog"
+    };
+  },
+  components: {
+    "main-nav": () => import("./components/navigation.js"),
+    "toast-noti": () => import("./components/toast.js")
+  },
+  created() {
+    console.log("Vue-App initialized: " + this.$data.title);
+  }
 });
 
 window.Application = {
-    VueApp: appvm,
-    Router: Router
+  VueApp: appvm,
+  Router: Router
 };
--- js/components/vue-study.js
+++ js/components/vue-study.js
@@ -1,26 +1,28 @@
 export default {
-    template: `<p>Template not found</p>`,
-    templateFile: "vue-study.html",
-    data() {
-        return {
-            name: "Vuejs componnet",
-            phoneNo: "098777888",
-        }
+  template: `<p>Template not found</p>`,
+  templateFile: "vue-study.html",
+  data() {
+    return {
+      logContent: ""
+    };
+  },
+  created() {
+    console.info("Vuejs compoent is initialized");
+    $.fn.jsScope = this.jsScope;
+  },
+  methods: {
+    run(tcCase) {
+      console.log(`-----'${tcCase}'---->`);
+      switch (tcCase) {
+        case "jsscope":
+          $.fn.jsScope();
+          break;
+
+        default:
+          break;
+      }
+      console.log(`<----'${tcCase}'-----`);
     },
-    created() {
-        console.info('Vuejs compoent is initialized');
-    },
-    computed: {
-        phoneNoChar() {
-            var message = "";
-            for (var i = 0; i < this.phoneNo.length; i++) {
-                if (this.phoneNo[i] == "1") { message += "Một "; } else if
-                    (this.phoneNo[i] == "2") { message += "Hai "; } else if (this.phoneNo[i] == "3") { message += "Ba "; } else if
-                        (this.phoneNo[i] == "4") { message += "Bốn "; } else if (this.phoneNo[i] == "5") { message += "Năm "; } else if
-                            (this.phoneNo[i] == "6") { message += "Sáu "; } else if (this.phoneNo[i] == "7") { message += "Bảy "; } else if
-                                (this.phoneNo[i] == "8") { message += "Tám "; } else if (this.phoneNo[i] == "9") { message += "Chín "; } else if
-                                    (this.phoneNo[i] == "0") { message += "Không "; }
-            } return message;
-        }
-    },
-}
+    jsScope() {}
+  }
+};
